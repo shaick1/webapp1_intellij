14b3aa57df9ba8e9f5a2f9effe713a1581c7256c
shaickmohamed Sirajudeen <shaick.pop@gmail.com>
2019-11-25 09:35:10 +0530

    Uploading all files

---
+++ src/main/java/com/mywebapp2/MyTestServlet.java
@@ -0,0 +1,30 @@
+package com.mywebapp2;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/firstservlet2")
+public class MyTestServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+        pw.println("My first servlet call - POST");
+        pw.close();
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+            response.setContentType("text/html");
+            PrintWriter pw = response.getWriter();
+            pw.println("My first servlet call - GET");
+            pw.close();
+
+    }
+}
---
+++ src/resources/index.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: shaickmohamedsirajudeen
+  Date: 10/11/19
+  Time: 7:14 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>My Servlet Example</title>
+  </head>
+  <body>
+  My first test page1
+  <a href="/mysamplewebproject2-1.0/firstservlet2"> Call GET Servlet</input>
+<br>
+      <form action="/mysamplewebproject2-1.0/firstservlet2" method="post">
+      <input type="submit">
+    </form>
+  </body>
+</html>
